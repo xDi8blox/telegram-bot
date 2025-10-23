f439fe8fd727aba0788ce6a808a100f57e394637
Death <61057790+xDi8blox@users.noreply.github.com>
2025-10-23 23:50:49 +0100

    Update index.js
    Fix vercel deployment code

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.get("/", (req, res) => res.send("Bot is live!"));
 
 app.post("/api/webhook", async (req, res) => {
   const data = req.body;
-  console.log(data);
 
   if (data.message) {
     const chatId = data.message.chat.id;
     const text = data.message.text || "";
+
     await fetch(`${TELEGRAM_API}/sendMessage`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ chat_id: chatId, text: `You said: ${text}` })
+      body: JSON.stringify({ chat_id: chatId, text: `You said: ${text}` }),
     });
   }
 
